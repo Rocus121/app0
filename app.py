4eea612142d447cc37ad8e379f071b190499b7dd
rocus121 <rm.miraglia@outlook.it>
2024-11-20 12:00:33 +0100

    aggiornamenti

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+def somma(l1:float, l2: float):
+    a = l1 + l2
+    return a 
+
+def main():
+    st.text("Ciao questo front-end funziona")
+    # slider
+    
+    num1 = st.slider('Please inserisci lato1 rettangolo', 0, 100, 20)
+    num2 = st.slider('Please inserisci lato2 rettangolo', 0, 100, 20)
+    r = somma(num1, num2)
+    
+    st.write("la somma è ", r)
+    
+if __name__ == "__main__":
+    main()    
